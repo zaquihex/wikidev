41459c08ff2a0feffffa0c57b8c4ade3215b6211
zaquiel.rodriguez <zaquiel.rodriguez@datiobbva.com>
2021-01-10 16:59:57 +0100

    questionRadioBtn completed - first version

--- src/app/react/RadioQuestion.tsx
+++ src/app/react/RadioQuestion.tsx
@@ -1,67 +1,51 @@
 import * as React  from 'react';
-
 // @ts-ignore
 import radioQuestionMkp from "@constants/mockups/questionRadioBtn.json"
 
 const RadioQuestion = () => {
+  // Update Radio buttons checked
   const [userSolution, setUserSolution] = React.useState(null);
+  // Show solution on radio options
   const [showSolution, setShowSolution] = React.useState(false);
+  // show question info
+  const [showInfo, setShowInfo] = React.useState(false);
+
 
   return (
-    <>
-      <span>{radioQuestionMkp.question}</span>
-      {
-        radioQuestionMkp.options.map(optionInfo => (
-          <div className="field">
-            <input type="radio" name={optionInfo.option} checked={userSolution === optionInfo.option} onClick={()=>{setUserSolution(optionInfo.option)}} />
-            <label>Option 1</label>
+      <div className="row">
+        <div className="col s12 m6">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">{radioQuestionMkp.question}</span>
+              <div className='flexColumn'>
+              {
+                radioQuestionMkp.options.map((optionInfo, posOption) => {
+                  const checkedOption = userSolution === posOption;
+                  const isColorGreen = showSolution && optionInfo.solution;
+                  const isColorRed = showSolution && checkedOption && !optionInfo.solution;
+                  const colorOption = isColorGreen ? 'colorGreen' : isColorRed ? 'colorRed' :  'colorWhite';
+                  return (
+                    <label>
+                      <input className="with-gap" name="group1" type="radio" checked={checkedOption} onClick={()=>{setUserSolution(posOption)}} />
+                      <span className={`${colorOption}`}>{optionInfo.option}</span>
+                    </label>
+                  );
+                })
+              }
+              </div>
+            </div>
+            <div className="card-action">
+              <div className="flex centerContainer" style={{maxHeight: '50px'}}>
+                <a className='btnCursor widthAuto' onClick={()=>{setShowInfo(!showInfo)}}>Show info</a>
+                <div className={`colorWhite transitionFade ${showInfo ? 'appear' : 'disappear'}`} style={{width: showInfo ? '70%' :  '0%'}}>
+                  {radioQuestionMkp.info}
+                </div>
+                <a className={`waves-effect waves-light btn marginSmall widthAuto transitionFade ${showInfo ? 'disappear' : 'appear'}`} onClick={()=>{setShowSolution(!showSolution)}}>{showSolution ? 'hide answer' : 'check answer'}</a>
+              </div>
+              </div>
           </div>
-        ))
-      }
-      <button onClick={()=>{setShowSolution(!showSolution)}}>{showSolution ? 'hide question' : 'check Question'}</button>
-      {showSolution ?
-        <div>
-          <h2>Solution</h2>
-          <div>{radioQuestionMkp.info}</div>
-        </div> : null}
-    </>)
+        </div>
+      </div>)
 }
 
 export default RadioQuestion;
-
-
-/*
-
-import * as React  from 'react';
-// @ts-ignore
-//import radioQuestionMkp from "../../constants/mockups/questionRadioBtn.json";
-import radioQuestionMkp from "@constants/mockups/questionRadioBtn.json"
-
-const RadioQuestion = () => {
-  const [userSolution, setUserSolution] = React.useState(null);
-  const [showSolution, setShowSolution] = React.useState(false);
-
-  return (
-    <>
-      <span>{radioQuestionMkp.question}</span>
-      {
-        radioQuestionMkp.options.map(optionInfo => (
-          <div className="field">
-            <input type="radio" name={optionInfo.option} checked={userSolution === optionInfo.option} onClick={()=>{setUserSolution(optionInfo.option)}} />
-            <label>Option 1</label>
-          </div>
-        ))
-      }
-      <button onClick={()=>{setShowSolution(!showSolution)}}>{showSolution ? 'hide question' : 'check Question'}</button>
-      {showSolution ?
-        <div>
-        <h2>Solution</h2>
-        <div>{radioQuestionMkp.info}</div>
-      </div> : null}
-    </>)
-}
-
-export default RadioQuestion;
-
-
- */
